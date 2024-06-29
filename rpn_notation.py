432de60e58c456fe4b43abc95dc47e75a59babaf
AndrysDM <andrysmoreno4@gmail.com>
2024-06-29 16:04:35 -0400

    a

---
+++ rpn_notation.py
@@ -0,0 +1,212 @@
+from const import *
+
+
+class RPN_Notation:
+    def __init__(self, tokens):
+        self.tokens = tokens
+
+
+def process_function_call(function_token_description, tokens):
+    token_type, function_name = function_token_description
+    assert token_type == TOKENS_TYPES.FUNCTION_CALL, "Error: token no es una llamada a function"
+    args = [[]]
+    rpar_count = 1
+    _token_type = None
+    while len(tokens) > 0:
+
+        current_arg = args[-1]
+
+        token_description = tokens.pop(0)
+        _token_type, _token_value = token_description
+
+        if _token_type == TOKENS_TYPES.ARGS_SEPARATOR:
+            assert rpar_count == 1, "Error: no se ha cerrado el paréntesis"
+            args.append([])
+            continue
+
+        elif _token_type == TOKENS_TYPES.LPAR:
+            rpar_count += 1
+
+        elif _token_type == TOKENS_TYPES.RPAR:
+            rpar_count -= 1
+            if rpar_count == 0:
+                token_type = TOKENS_TYPES.FUNCTION_CALL
+
+                if function_name in USERS_FUNCTIONS:
+                    function_description = USERS_FUNCTIONS[function_name]
+                    assert len(function_description['func_params']) == len(
+                        args), "Error: cantidad de parámetros incorrecta"
+                else:
+                    assert function_name in BUILD_IN_FUNCTIONS, "Error: función no definida"
+                    assert len(
+                        args) == 1, "Error: cantidad de parámetros incorrecta"
+                    token_type = TOKENS_TYPES.BUILD_IN_FUNCTION_CALL
+                break
+        elif _token_type == TOKENS_TYPES.FUNCTION_CALL:
+            current_arg.append(process_function_call(
+                token_description, tokens))
+            continue
+        current_arg.append(token_description)
+
+    assert _token_type == TOKENS_TYPES.RPAR, "Error: no se ha cerrado el paréntesis"
+    return RPN_Notation([
+        token_description
+        for arg in args
+        if len(arg) > 0
+        for token_description in shunting_yard(arg).tokens
+    ] + [(token_type, function_name)])
+
+
+def shunting_yard(tokens):
+    output_deque, ops_stack = [], []
+
+    tokens_copy = [*tokens]
+
+    while len(tokens_copy) > 0:
+
+        ix = len(tokens) - len(tokens_copy)
+        token_description = tokens_copy.pop(0)
+
+        if isinstance(token_description, RPN_Notation):
+            output_deque += token_description.tokens
+            continue
+
+        (token_type, token_value) = token_description
+
+        assert token_type != TOKENS_TYPES.ARGS_SEPARATOR, "Error: token desconocido"
+
+        if token_type in {
+            TOKENS_TYPES.FLOAT,
+            TOKENS_TYPES.INT
+        }:
+            output_deque.append((token_type, float(token_value)))
+
+        elif token_type == TOKENS_TYPES.IDENT:
+            output_deque.append(token_description)
+
+        elif token_type == TOKENS_TYPES.SYMBOL:
+
+            if token_value == '-':
+                if ix == 0:
+                    token_value = 'neg'
+
+                if tokens[ix - 1][0] not in {
+                    TOKENS_TYPES.FLOAT,
+                    TOKENS_TYPES.INT,
+                    TOKENS_TYPES.IDENT
+                }:
+                    token_value = 'neg'
+
+            if len(ops_stack) == 0:
+                ops_stack.append((token_type, token_value))
+                continue
+            last_op = ops_stack[-1]
+            last_op_type, last_op_value = last_op
+
+            if last_op_type == TOKENS_TYPES.LPAR:
+                ops_stack.append(token_description)
+                continue
+
+            curr_precedence, curr_associativity, _, _ = OPERATORS[token_value]
+            last_precedence, last_associativity, _, _ = OPERATORS[last_op_value]
+
+            while len(ops_stack) > 0 and ((
+                curr_associativity == 'L' and curr_precedence <= last_precedence)
+                or (curr_associativity == 'R' and curr_precedence < last_precedence)
+            ):
+                output_deque.append(ops_stack.pop())
+
+            ops_stack.append((token_type, token_value))
+
+        elif token_type == TOKENS_TYPES.FUNCTION_CALL:
+            output_deque.append(
+                process_function_call(
+                    token_description,
+                    tokens_copy
+                )
+            )
+
+        elif token_type == TOKENS_TYPES.LPAR:
+            ops_stack.append(token_description)
+        elif token_type == TOKENS_TYPES.RPAR:
+            lpar_open = 1
+            while len(ops_stack) > 0:
+                token_last_op = ops_stack.pop()
+                _token_type, _ = token_last_op
+                if _token_type == TOKENS_TYPES.LPAR:
+                    lpar_open -= 1
+                    break
+                output_deque.append(token_last_op)
+            assert lpar_open == 0, "Error: no se ha cerrado el paréntesis"
+
+    output_deque += ops_stack[::-1]
+
+    # return RPN_Notation([output_deque])
+    return RPN_Notation(
+        [
+            token
+            for output in output_deque
+            for token in (
+                output.tokens
+                if isinstance(output, RPN_Notation)
+                else [output]
+            )
+        ]
+    )
+
+
+# Diccionario de constantes matemáticas
+def evaluate_function(func, args):
+    params = func['func_params']
+    body = func['func_body']
+    if len(params) != len(args):
+        raise ValueError("Numero incorrecto de argumentos")
+    return evaluate_postfix(
+        body,
+        {
+            k: v for k, v in zip(params, args)
+        })
+
+
+def evaluate_postfix(rpn_notation, vars):
+    postfix = [*rpn_notation]
+    stack = []
+    while postfix:
+        token_type, token = postfix.pop(0)
+        if token_type in {
+            TOKENS_TYPES.FLOAT,
+            TOKENS_TYPES.INT
+        }:
+            stack.append(token)
+        elif token_type == TOKENS_TYPES.IDENT:
+            if token in CONSTANTS:
+                stack.append((CONSTANTS[token]))
+            else:
+                stack.append(vars[token])
+                
+        elif token_type == TOKENS_TYPES.FUNCTION_CALL:
+
+            func_name = token
+            assert func_name in USERS_FUNCTIONS, "Error: función no definida"
+
+            function_descriptor = USERS_FUNCTIONS[func_name]
+            arg_count = function_descriptor['func_params']
+            args = [stack.pop() for _ in range(len(arg_count))]
+            stack.append(
+                evaluate_function(
+                    USERS_FUNCTIONS[func_name], args[::-1]
+                )
+            )
+
+        elif token_type in {
+            TOKENS_TYPES.SYMBOL,
+            TOKENS_TYPES.BUILD_IN_FUNCTION_CALL
+        }:
+            _, _, arity, func = OPERATORS[token]
+            args = [stack.pop() for _ in range(arity)]
+            stack.append(func(*args[::-1]))
+        else:
+            raise ValueError(f"Token desconocido: {token}")
+
+    assert len(stack) == 1, "Error: expresion invalida"
+    return stack[0]
--- tools.py
+++ tools.py
@@ -10,9 +10,10 @@ import numpy as np
 from scipy.interpolate import griddata
 from matplotlib.animation import FuncAnimation, PillowWriter
 from collections import OrderedDict
+from rpn_notation import evaluate_function, evaluate_postfix, shunting_yard
+from const import *
 
-
-rango_x = [0, np.pi, 0.05]
+rango_x = [0, 10, 1]
 rango_y = [0, np.pi, 0.05]
 rango_z = [0,1,0.1]
 core = 5
@@ -21,34 +22,6 @@ kind = "plot"
 polar = False
 
 # Diccionario de operadores con su precedencia, asociatividad y funcion correspondiente
-OPERATORS = {
-    '+': (1, 'L', operator.add),
-    '-': (1, 'L', operator.sub),
-    '*': (2, 'L', operator.mul),
-    '/': (2, 'L', operator.truediv),
-    '^': (3, 'R', operator.pow),
-    'neg': (4, 'R', operator.neg)
-}
-
-# Diccionario de funciones matematicas
-FUNCTIONS = {
-    'sin': math.sin,
-    'cos': math.cos,
-    'tan': math.tan,
-    'cot': lambda x: 1 / math.tan(x),
-    'log': math.log,
-    'log10': math.log10,
-    'sqrt': math.sqrt
-}
-
-# Diccionario de constantes matematicas
-CONSTANTS = {
-    'pi': math.pi,
-    'e': math.e
-}
-
-func_defs = {}
-
 
 def parse_function_definition(func_def):
     match = re.match(
@@ -62,126 +35,43 @@ def parse_function_definition(func_def):
     idents = {text for t_name, text in tokens if t_name == 'ident'}
 
     undefined = idents - set(params) - set(OPERATORS) - \
-        set(FUNCTIONS) - set(CONSTANTS)
+        set(BUILD_IN_FUNCTIONS) - set(CONSTANTS)
     if undefined:
         raise ValueError(f"Parametros no definidos: {', '.join(undefined)}")
 
-    _shunting_yard = shunting_yard(tokens)
+    _shunting_yard = shunting_yard(tokens).tokens
 
     return name, params, _shunting_yard
 
 
-def shunting_yard(tokens):
-    output, ops = deque(), []
-    arg_count_stack = []
-
-    for t_name, token in tokens:
-        if t_name in {'float', 'int'}:
-            output.append(float(token))
-        elif t_name == 'ident':
-            output.append(token)
-        elif t_name == 'function_call':
-            func_name = token[:-1]
-            if func_name in FUNCTIONS or func_name in func_defs:
-                ops.append(func_name)
-                arg_count_stack.append(0)
-                ops.append('(')
-            else:
-                print([*func_defs.keys()])
-                raise ValueError(f"Token desconocido: {func_name}")
-        elif token == ',':
-            while ops and ops[-1] != '(':
-                output.append(ops.pop())
-            if arg_count_stack:
-                arg_count_stack[-1] += 1
-        elif token in OPERATORS:
-            if (not output or output[-1] in OPERATORS or output[-1] in {'(', ')'}) and token == '-':
-                token = 'neg'
-            while (ops and ops[-1] in OPERATORS and
-                    ((OPERATORS[token][1] == 'L' and OPERATORS[token][0] <= OPERATORS[ops[-1]][0]) or
-                     (OPERATORS[token][1] == 'R' and OPERATORS[token][0] < OPERATORS[ops[-1]][0]))):
-                output.append(ops.pop())
-            ops.append(token)
-        elif token == '(':
-            ops.append(token)
-        elif token == ')':
-            while ops and ops[-1] != '(':
-                output.append(ops.pop())
-            ops.pop()
-            if ops and ops[-1] not in OPERATORS and ops[-1] not in {'(', ')'}:
-                func = ops.pop()
-                arg_count = arg_count_stack.pop() + 1
-                output.append((func, arg_count))
-
-    while ops:
-        output.append(ops.pop())
-
-    return output
-
-
-def evaluate_postfix(postfix, vars):
-    stack = []
-    while postfix:
-        token = postfix.popleft()
-        if isinstance(token, float):
-            stack.append(token)
-        elif isinstance(token, tuple):
-            func_name, arg_count = token
-            args = [stack.pop() for _ in range(arg_count)]
-            if func_name in func_defs:
-                stack.append(evaluate_function(
-                    func_defs[func_name], args[::-1]))
-            else:
-                stack.append(FUNCTIONS[func_name](*args[::-1]))
-        elif token in vars:
-            stack.append(vars[token])
-        elif token in CONSTANTS:
-            stack.append(CONSTANTS[token])
-        elif token in OPERATORS:
-            if len(stack) < 2 and token != 'neg':
-                raise ValueError(
-                    f"Error: no hay suficientes operandos para el operador '{token}'")
-            b = stack.pop()
-            a = stack.pop() if token != 'neg' else 0
-            stack.append(OPERATORS[token][2](a, b))
-        else:
-            raise ValueError(f"Token desconocido: {token}")
-
-    return stack[0]
-
-
 def pre_process_expression(expression):
     matches = re.finditer(
-        r'(?P<float>-?\d+\.\d+)|(?P<int>-?\d+)|(?P<function_call>\w+\()|(?P<LPAR>\()|(?P<RPAR>\))|(?P<ident>\w+)|(?P<symbol>[+\-*/^])|(?P<args_separator>,)|(?:\s*)',
+        PATTERN,
         expression
     )
 
     tokens = [
-        (token.lastgroup, token.group())
+        (TOKENS_TYPES(token.lastgroup), token.group())
         for token in matches
         if token.lastgroup is not None
+    ]
+
+    tokens = [
+        (t_type, t_value[:-1])
+        if t_type == TOKENS_TYPES.FUNCTION_CALL
+        else (t_type, t_value)
+        for (t_type, t_value)
+        in tokens
     ]
     return tokens
 
 
-def full_evaluate_expression(expression, vars):
-
-    tokens = pre_process_expression(expression)
-    _shunting_yard = shunting_yard(tokens)
-
-    return evaluate_postfix(_shunting_yard, vars)
-
-
 def evaluate_expression(expression, vars):
 
-    return evaluate_postfix(expression, vars)
+    tokens = pre_process_expression(expression)
+    _shunting_yard = shunting_yard(tokens).tokens
 
-
-def evaluate_function(func, args):
-    params, body = func
-    if len(params) != len(args):
-        raise ValueError("Numero incorrecto de argumentos")
-    return evaluate_expression(body, dict(zip(params, args)))
+    return evaluate_postfix(_shunting_yard, vars)
 
 
 def log_interaction(log_file, interaction):
@@ -189,24 +79,39 @@ def log_interaction(log_file, interaction):
         f.write(interaction + '\n')
 
 
-func_defs = {}
+def _handle_gen(args):
+    func_name = args[0]
+    args = args[1]
+    func_description = USERS_FUNCTIONS[func_name]
+    return evaluate_function(func_description, args)
+
+def save_database():
+    print("Guardando base de datos")
+    print(f"Funciones definidas: {[*USERS_FUNCTIONS.keys()]}")
+
+    with open(f'.save_functions.txt', mode='wb') as f:
+        pickle.dump(USERS_FUNCTIONS, f)
+
 
 
 def handle_define(func_def):
+
+    func_name, func_params, func_body = parse_function_definition(func_def)
+
     try:
-        func_name, func_params, func_body = parse_function_definition(func_def)
-        func_defs[func_name] = (func_params, func_body)
-        print(f"Funcion '{func_name}' definida exitosamente.")
-        return f"Funcion '{func_name}' definida exitosamente."
+        USERS_FUNCTIONS[func_name] = dict(
+            func_params=func_params,
+            func_body=func_body,
+            func_def=func_def
+        )
+        print(f"Función '{func_name}' definida exitosamente.")
     except ValueError as e:
-        return f"Error: {e}"
+        print(f"Error al definir la función '{func_name}': {e}")
 
 
 def handle_evaluate(expression):
     try:
-        result = full_evaluate_expression(expression, {})
-        print(result)
-        return result
+        return evaluate_expression(expression, {})
     except (ValueError, ZeroDivisionError) as e:
         return f"Error: {e}"
 
@@ -216,17 +121,6 @@ def handle_list(func_defs):
         return "Funciones definidas:\n" + "\n".join(f"{name}({', '.join(params)}) = {body}" for name, (params, body) in func_defs.items())
     else:
         return "Aun no hay funciones definidas."
-
-
-def _handle_gen(args):
-    func_name = args[0]
-    args = args[1]
-    #print(f"Generando grafico de {func_name} con parametros {args}")
-    load_database()
-
-    func_desciption = func_defs[func_name]
-
-    return evaluate_function(func_desciption, args)
 
 
 def handle_config():
@@ -250,19 +144,8 @@ def handle_config():
         polar = False
 
 
-
-def save_database():
-    with open(f'.save_functions.txt', mode='wb') as f:
-        pickle.dump(func_defs, f)
-
-
-def load_database():
-    global func_defs
-    with open(f'.save_functions.txt', mode='rb') as f:
-        func_defs = pickle.load(f)
-
 import os
-def handle_gen(func, *args):
+def handle_gen(function_name):
     if os.path.exists('figure.png'):
         # Si el archivo existe, eliminarlo
         os.remove('figure.png')
@@ -271,10 +154,14 @@ def handle_gen(func, *args):
         os.remove('figure.gif')
     save_database()
 
-    (args, _) = func_defs[func]
+    function_description = USERS_FUNCTIONS[function_name]
+    params_definition = function_description['func_params']
+    body = function_description['func_body']
+    func_def = function_description['func_def']
+
     params = []
     rangos = [rango_x,rango_y, rango_z]
-    for i in range(len(args)):
+    for i in range(len(params_definition)):
         params.append(
             list(
                 np.arange(
@@ -284,15 +171,15 @@ def handle_gen(func, *args):
                 )
             )
         )
-    if len(args) == 1:
+    
+    if len(params_definition) == 1:
         params_1 = params[0]
-
         result = []
         with Pool(core) as p:
             result = p.map(
                 _handle_gen,
                 [
-                    (func, [param_1])
+                    (function_name, [param_1])
                     for param_1 in params_1
                 ]
             )
@@ -311,8 +198,7 @@ def handle_gen(func, *args):
         fig.savefig("figure.png")
         print("guarde el grafico correctamente")
         plt.show()
-    elif len(args) == 2:
-        print('LLegue')
+    elif len(params_definition) == 2:
         x , y = params
         params_1=[]
         for i in x:
@@ -323,7 +209,7 @@ def handle_gen(func, *args):
             result = p.map(
                 _handle_gen,
                 [
-                    (func, param_1)
+                    (function_name, param_1)
                     for param_1 in params_1
                 ]
             )
@@ -388,7 +274,7 @@ def handle_gen(func, *args):
             ani.save('figure.gif', writer=PillowWriter(fps=20))
             print('guarde la animacion correctamente')
             plt.show()
-    elif len(args)==3:
+    elif len(params_definition)==3:
         x , y, z = params
         params_1=[]
         for i in x:
@@ -400,7 +286,7 @@ def handle_gen(func, *args):
             result = p.map(
                 _handle_gen,
                 [
-                    (func, param_1)
+                    (function_name, param_1)
                     for param_1 in params_1
                 ]
             )
